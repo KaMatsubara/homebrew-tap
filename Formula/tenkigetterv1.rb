20bb2dec45f421bed7d94e3363269cf4a75a7811
KaMatsubara <106413649+KaMatsubara@users.noreply.github.com>
2023-07-17 09:09:19 +0900

    Create tenkigetterv1.rb

---
+++ Formula/tenkigetterv1.rb
@@ -0,0 +1,18 @@
+class Tenkigetterv1 < Formula
+  desc "天気予報を取得する"
+  homepage "https://github.com/KaMatsubara/tenkiGetter"
+  url "https://github.com/KaMatsubara/tenkiGetter/archive/refs/tags/v1.0.tar.gz"
+  sha256 "99ec9b9ffe2c1706fa48c25e8bf44eec3b570667e599bdb5dc79b5f48dc6cef4"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install ”tenkiGetter"
+    bash_completion.install "completions/bash/tenkiGetter" if build.with? "completions"
+  end
+
+  test do
+    system bin/”tenkiGetter", "--version"
+  end
+end
